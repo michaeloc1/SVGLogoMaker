9f4b0fb00fefbc174ed12d628c5787f160a1e24a
michaeloc1 <michaeloc@verizon.net>
2023-03-03 16:54:43 -0500

    added to test.js

--- utils/generateSVG.js
+++ utils/generateSVG.js
@@ -5,8 +5,14 @@ const Circle = require("../lib/shapes.js")
 const generateSVG = (data) => {
    // console.log("hello")
    // console.log(data)
-   let newCircle = new Circle(data.text, data.textColor, data.shapeColor);
-    console.log(newCircle.render())
+   //let newCircle = new Circle(data.text, data.textColor, data.shapeColor);
+   //newCircle.shapeColor = "red"
+   // console.log(newCircle.render())
+   let aCircle = new Circle();
+   aCircle.text= "YOU"
+   aCircle.textColor = "gold"
+   aCircle.shapeColor = "black"
+   console.log(aCircle.render())
 
 }
 
